5ee8e7d5e6dcb8ac68ce03e2747ed8392d9e40e7
Raimondas Rimkevičius <github@mekdrop.name>
2025-08-10 03:09:32 +0300

    Update src/Exceptions/SpecifiedDataTypeNotFoundException.php
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/Exceptions/SpecifiedDataTypeNotFoundException.php
+++ src/Exceptions/SpecifiedDataTypeNotFoundException.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Imponeer\Properties\Exceptions;
 
@@ -7,13 +8,12 @@ use Throwable;
 
 class SpecifiedDataTypeNotFoundException extends Exception
 {
-
-    public function __construct(int $dataType, int $code = 0, ?Throwable $previous = null)
-	{
-		parent::__construct(
-			sprintf('Class for data type #%d not found!', $dataType),
-			$code,
-			$previous
-		);
-	}
+    public function __construct(public readonly int $dataType, int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct(
+            sprintf('Class for data type #%d not found!', $this->dataType),
+            $code,
+            $previous
+        );
+    }
 }
